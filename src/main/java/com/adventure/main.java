e88e25b808fe4e088b579cb884b28eed927bf1ff
Colin Hill <colin@peak2peakmedia.com>
2016-09-27 14:07:47 +0200

    [Add] Webserver working :: Launch at http://localhost:4567/hello

--- src/main/java/com/adventure/main.java
+++ src/main/java/com/adventure/main.java
@@ -4,5 +4,12 @@ package com.adventure;
  * Created by chill on 9/27/16.
  */
 
+import static  spark.Spark.*;
 
+public class main {
 
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+}
