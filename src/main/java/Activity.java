7cdc843fda7981b2b669d89bc9b1dabc59a8630b
REX500 <filamalek@hotmail.com>
2016-09-29 11:16:11 +0200

    fil commit

--- src/main/java/Activity.java
+++ src/main/java/Activity.java
@@ -1,86 +1,69 @@
 /**
-* Created by Filip and Colin on 29-09-2016.
-*/
+ * Created by Filip on 29-09-2016.
+ */
 public class Activity {
-   private String name;
-   private String place;
-   private int capacity;
-   private int minAge;
-   private double price;
-   private int time;
+    private String name;
+    private String place;
+    private int capacity;
+    private int minAge;
+    private double price;
+    private int time;
 
-   private static ArrayList<Activity> instances = new ArrayList<Activity>();
-  private int id;
+    public String getName() {
+        return name;
+    }
 
-   public String getName() {
-       return name;
-   }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-   public void setName(String name) {
-       this.name = name;
-   }
+    public String getPlace() {
+        return place;
+    }
 
-   public String getPlace() {
-       return place;
-   }
+    public void setPlace(String place) {
+        this.place = place;
+    }
 
-   public void setPlace(String place) {
-       this.place = place;
-   }
+    public int getCapacity() {
+        return capacity;
+    }
 
-   public int getCapacity() {
-       return capacity;
-   }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
-   public void setCapacity(int capacity) {
-       this.capacity = capacity;
-   }
+    public int getMinAge() {
+        return minAge;
+    }
 
-   public int getMinAge() {
-       return minAge;
-   }
+    public void setMinAge(int minAge) {
+        this.minAge = minAge;
+    }
 
-   public void setMinAge(int minAge) {
-       this.minAge = minAge;
-   }
+    public double getPrice() {
+        return price;
+    }
 
-   public double getPrice() {
-       return price;
-   }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-   public void setPrice(double price) {
-       this.price = price;
-   }
+    public int getTime() {
+        return time;
+    }
 
-   public int getTime() {
-       return time;
-   }
-
-   public void setTime(int time) {
-       this.time = time;
-   }
-
-   public static List<Activity> all() {
-     return instances;
-  }
-
-  public static void clear() {
-    instances.clear();
-  }
-
-  public int getId() {
-    return mId;
-  }
+    public void setTime(int time) {
+        this.time = time;
+    }
 
 
-
-   public Activity(String name, String place, double price, int time, int capacity, int minAge){
-       this.name = name;
-       this.place = place;
-       this.price = price;
-       this.time = time;
-       this.capacity = capacity;
-       this.minAge = minAge;
-       this.id = instances.size();
-   }
+    public Activity(String name, String place, double price, int time, int capacity, int minAge){
+        this.name = name;
+        this.place = place;
+        this.price = price;
+        this.time = time;
+        this.capacity = capacity;
+        this.minAge = minAge;
+    }
 }
--- src/test/java/ActivityTests.java
+++ src/test/java/ActivityTests.java
@@ -2,18 +2,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
-* Created by Filip and Colin on 29-09-2016.
-*/
+ * Created by Filip on 29-09-2016.
+ */
 public class ActivityTests {
 
-   @Test
-   public void Activity_isCreatedCorrectly(){
-       Activity testActivity = new Activity("Paintball", "Cph", 500, 1500, 15, 14);
-       assertEquals("Paintball", testActivity.getName());
-       assertEquals("Cph", testActivity.getPlace());
-       //assertEquals(500, testActivity.getPrice());
-       assertEquals(1500, testActivity.getTime());
-       assertEquals(15, testActivity.getCapacity());
-       assertEquals(14, testActivity.getMinAge());
-   }
+    @Test
+    public void Activity_isCreatedCorrectly(){
+        Activity testActivity = new Activity("Paintball", "Cph", 500, 1500, 15, 14);
+        assertEquals("Paintball", testActivity.getName());
+        assertEquals("Cph", testActivity.getPlace());
+        //assertEquals(500, testActivity.getPrice());
+        assertEquals(1500, testActivity.getTime());
+        assertEquals(15, testActivity.getCapacity());
+        assertEquals(14, testActivity.getMinAge());
+    }
 }
