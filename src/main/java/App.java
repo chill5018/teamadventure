69deb12f9dabe7daac2dcf3ade3e1d1875bf0285
Denis Kutnár <deniskutnar@MacBook-Pro-uzivatela-Denis.local>
2016-10-06 10:44:54 +0200

    All fields of previous booking are editable

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -140,6 +140,13 @@ public class App {
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
 
+//Edit Booking
+  get("/bookings/edit", (request, response) -> {
+   HashMap<String, Object> model = new HashMap<String, Object>();
+   model.put("template", "templates/bookings-edit.vtl");
+   return new ModelAndView(model, layout);
+ }, new VelocityTemplateEngine());
+
   // // View Booking with ID
   // get("/bookings/:id", (request, response) -> {
   //     HashMap<String, Object> model = new HashMap<String, Object>();
